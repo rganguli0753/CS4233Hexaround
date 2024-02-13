dc1218455ef51899e9621eea2b27c2d4b083317f
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2024-02-13 17:33:28 -0500

    Adding methods for Board

--- HexAroundC15-sub1/src/hexaround/game/gameBoard.java
+++ HexAroundC15-sub1/src/hexaround/game/gameBoard.java
@@ -11,7 +11,24 @@ public class gameBoard {
         return hexBoard;
     }
     public void placePiece(Hex coord){
+        hexBoard.add(coord);
+    }
 
+    public boolean isOccupied(int x, int y){
+        Hex checkCoord = new Hex(x,y);
+        for(Hex coord: this.hexBoard)
+            if(coord.getDistance(checkCoord)==0)
+                return false;
+        return true;
+    }
+
+    public Hex getHex(int x, int y){
+        Hex spot = new Hex(x,y);
+        for(Hex coords: this.hexBoard){
+            if (coords.getDistance(spot)==0)
+                return spot;
+        }
+        return null;
     }
 
 
