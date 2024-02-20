4aef25d744d9e1df0de8ca22581b5b9251180dc9
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2024-02-19 21:08:36 -0500

    Move method to make more sense

--- HexAroundC15-sub1/src/hexaround/game/HexAroundFirstSubmission.java
+++ HexAroundC15-sub1/src/hexaround/game/HexAroundFirstSubmission.java
@@ -141,9 +141,16 @@ public class HexAroundFirstSubmission implements IHexAround1{
     @Override
     public MoveResponse moveCreature(CreatureName creature, int fromX, int fromY, int toX, int toY) {
         int creaturedist = creatureInf.get(getCreatureAt(fromX,fromY)).maxDistance();
+        if(!isOccupied(fromX,fromY))
+            return new MoveResponse(MoveResult.MOVE_ERROR,"PIECE IS MISSING");
+        if(isOccupied(toX,toY))
+            return new MoveResponse(MoveResult.MOVE_ERROR,"SPOT IS OCCUPIED");
+        if(!board.reachable(new Hex(fromX, fromY), new Hex(toX, toY), creaturedist))
+            return new MoveResponse(MoveResult.MOVE_ERROR,"SPOT TOO FAR");
         if(creature!=getCreatureAt(fromX,fromY))
             return new MoveResponse(MoveResult.MOVE_ERROR,"INCORRECT CREATURE MOVEMENT");
-        return board.moveCreature(creature,fromX,fromY,toX,toY, creaturedist);
+        
+        return new MoveResponse(MoveResult.OK);
     }
 }
 
